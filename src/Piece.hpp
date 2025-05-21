f33937a01e7e6b7133309ae50daeb1b1f483f99d
Lucas Pujol <lucas.pujol@epitech.eu>
2025-05-21 16:14:46 +0200

    feat(Piece): add default constructor and getType method to Piece class

--- src/Piece.hpp
+++ src/Piece.hpp
@@ -8,13 +8,16 @@ class Piece {
         enum Tetromino { I, O, T, S, Z, J, L };
         using Shape = std::array<std::array<int, 5>, 5>;
 
+        Piece() : Piece(I) {}
         Piece(Tetromino type);
         void rotate();
         const Shape &getShape() const;
+        char getType() const;
     
     private:
         std::array<Shape, 4> rotations;
         int currentRotation;
+        Tetromino tetrominoType;
 };
 
 #endif /* _PIECE_ */
