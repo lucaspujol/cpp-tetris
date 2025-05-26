6e929e449d9e861c4c81be2cbf53227c26259248
Lucas Pujol <lucas.pujol@epitech.eu>
2025-05-26 18:33:50 +0200

    fix(Renderer): correct header guard formatting and update drawBoard method name for consistency

--- src/Renderer.hpp
+++ src/Renderer.hpp
@@ -1,8 +1,10 @@
 #ifndef _RENDERER_
-    #define _RENDERER_
+#define _RENDERER_
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
+#include <vector>
+#include <string>
 
 #include "Board.hpp"
 #include "Piece.hpp"
@@ -13,15 +15,31 @@ class Renderer {
     public:
         Renderer(SDL_Renderer *r);
         ~Renderer();
-        void draw_board(const Board &board, const Piece &piece, int x, int y, const std::vector<Piece> &nextPieces);
+        void drawBoard(const Board &board, const Piece &piece, int x, int y, const std::vector<Piece> &nextPieces);
 
     private:
         SDL_Renderer *renderer;
         TTF_Font *font;
         const int blockSize = 45;
+
         void drawPiece(const Piece &piece, int offsetX, int offsetY, int size, bool isGhost = false);
         void setPieceColor(char pieceType, bool isGhost = false);
         void renderText(const char* text, SDL_Rect destRect, SDL_Color color = {255, 255, 255, 255});
+        void renderTextWithSize(const char* text, SDL_Rect destRect, SDL_Color color, int fontSize);
+        void drawBoardGrid(const Board &board, int offsetX, int offsetY);
+        void drawGhostPiece(const Board &board, const Piece &piece, int posX, int posY, int offsetX = 0, int offsetY = 0);
+        void drawNextPiecesPanel(const std::vector<Piece> &nextPieces, int panelX, int panelY, int nextPieceSize);
+        void drawScorePanel(int score, int level);
+
+        // Score panel helper methods
+        float calculateScorePulseIntensity(int score);
+        SDL_Rect calculateScorePanelPosition();
+        void drawScorePanelBackground(const SDL_Rect& scorePanel, float pulseIntensity);
+        void drawScorePanelBorders(const SDL_Rect& scorePanel);
+        void drawScoreSection(const SDL_Rect& scorePanel, int score, float pulseIntensity);
+        void drawDivider(const SDL_Rect& scorePanel);
+        void drawLevelSection(const SDL_Rect& scorePanel, int level);
+        void drawLevelIndicatorDots(const SDL_Rect& scorePanel, int level);
 };
 
 #endif /* _RENDERER_ */
